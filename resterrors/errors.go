2e2bc503da25e036e9889570fc6af269b94b8e6c
Diego Clair <diego93rodrigues@gmail.com>
2020-04-25 00:05:05 -0300

    Create errors handle

---
+++ resterrors/errors.go
@@ -0,0 +1,37 @@
+package resterrors
+
+import "net/http"
+
+// RestErr struct
+type RestErr struct {
+	Message    string `json:"message"`
+	StatusCode int    `json:"status_code"`
+	Error      string `json:"error"`
+}
+
+// NewBadRequestError returns a bad_request code error with string message error
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message:    message,
+		StatusCode: http.StatusBadRequest,
+		Error:      "bad_request",
+	}
+}
+
+// NewNotFoundError returns a not_found code error with string message error
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message:    message,
+		StatusCode: http.StatusNotFound,
+		Error:      "not_found",
+	}
+}
+
+// NewInternalServerError returns a not_found code error with string message error
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message:    message,
+		StatusCode: http.StatusInternalServerError,
+		Error:      "internal_server_error",
+	}
+}
---
+++ resterrors/errors_test.go
@@ -0,0 +1,30 @@
+package resterrors
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewBadRequestError(t *testing.T) {
+	err := NewBadRequestError("this is the message")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusBadRequest, err.StatusCode)
+	assert.EqualValues(t, "this is the message", err.Message)
+}
+
+func TestNewNotFoundError(t *testing.T) {
+	err := NewNotFoundError("this is the message")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "this is the message", err.Message)
+}
+
+func TestNewInternalServerError(t *testing.T) {
+	err := NewInternalServerError("this is the message")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
+	assert.EqualValues(t, "this is the message", err.Message)
+	assert.EqualValues(t, "internal_server_error", err.Error)
+}
