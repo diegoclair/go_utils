cf040575476058638ff51c7c48710d100071b380
Diego Clair <diego93rodrigues@gmail.com>
2020-07-12 17:20:23 -0300

    Fix resterr struct

--- resterrors/errors.go
+++ resterrors/errors.go
@@ -13,63 +13,63 @@ type RestErr interface {
 }
 
 type restErr struct {
-	message    string `json:"message"`
-	statusCode int    `json:"status_code"`
-	error      string `json:"error"`
+	ErrMessage    string `json:"message"`
+	ErrStatusCode int    `json:"status_code"`
+	ErrError      string `json:"error"`
 }
 
 func (e restErr) Message() string {
-	return e.message
+	return e.ErrMessage
 }
 func (e restErr) StatusCode() int {
-	return e.statusCode
+	return e.ErrStatusCode
 }
 
 func (e restErr) Error() string {
-	return fmt.Sprintf("message: %s - statusCode: %d - error: %s", e.message, e.statusCode, e.error)
+	return fmt.Sprintf("message: %s - statusCode: %d - error: %s", e.ErrMessage, e.ErrStatusCode, e.ErrError)
 }
 
 // NewRestError returns a instace of type restErr
 func NewRestError(message string, status int, err string) RestErr {
 	return restErr{
-		message:    message,
-		statusCode: status,
-		error:      err,
+		ErrMessage:    message,
+		ErrStatusCode: status,
+		ErrError:      err,
 	}
 }
 
 // NewBadRequestError returns a bad_request code error with your string message error
 func NewBadRequestError(message string) RestErr {
 	return restErr{
-		message:    message,
-		statusCode: http.StatusBadRequest,
-		error:      "bad_request",
+		ErrMessage:    message,
+		ErrStatusCode: http.StatusBadRequest,
+		ErrError:      "bad_request",
 	}
 }
 
 // NewNotFoundError returns a not_found code error with your string message error
 func NewNotFoundError(message string) RestErr {
 	return restErr{
-		message:    message,
-		statusCode: http.StatusNotFound,
-		error:      "not_found",
+		ErrMessage:    message,
+		ErrStatusCode: http.StatusNotFound,
+		ErrError:      "not_found",
 	}
 }
 
 // NewInternalServerError returns a not_found code error with your string message error
 func NewInternalServerError(message string) RestErr {
 	return &restErr{
-		message:    message,
-		statusCode: http.StatusInternalServerError,
-		error:      "internal_server_error",
+		ErrMessage:    message,
+		ErrStatusCode: http.StatusInternalServerError,
+		ErrError:      "internal_server_error",
 	}
 }
 
 // NewUnauthorizedError returns a unauthorized code error with your string message error
 func NewUnauthorizedError(message string) RestErr {
 	return &restErr{
-		message:    message,
-		statusCode: http.StatusUnauthorized,
-		error:      "unauthorized",
+		ErrMessage:    message,
+		ErrStatusCode: http.StatusUnauthorized,
+		ErrError:      "unauthorized",
 	}
 }
