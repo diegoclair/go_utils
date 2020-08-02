b57b77c8c56281fbb4cf192d2b54e27c713dd158
Diego Clair <diego93rodrigues@gmail.com>
2020-08-01 23:48:52 -0300

    Created mysqlutils library

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/stretchr/testify v1.6.1
 	go.uber.org/zap v1.15.0
+	github.com/go-sql-driver/mysql v1.5.0
 )
---
+++ mysqlutils/mysql.go
@@ -0,0 +1,77 @@
+package mysqlutils
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/diegoclair/go_utils-lib/resterrors"
+	"github.com/go-sql-driver/mysql"
+)
+
+const (
+	errorNoRows       = "no rows in result set"
+	duplicatedKeyCode = 1062
+)
+
+// HandleMySQLError - handle mysql errors
+func HandleMySQLError(err error) resterrors.RestErr {
+
+	//if exists the error on mysql.MySQLError
+	sqlErr, exists := err.(*mysql.MySQLError)
+	if !exists {
+		if strings.Contains(err.Error(), errorNoRows) {
+			return resterrors.NewNotFoundError("No records find with the parameters")
+		}
+		return resterrors.NewInternalServerError(
+			fmt.Sprintf("Error database response: %s", err.Error()))
+	}
+
+	switch sqlErr.Number {
+	case duplicatedKeyCode:
+		duplicatedKey := between(sqlErr.Message, "key '", "_UNIQUE")
+		duplicatedKeyValue := between(sqlErr.Message, "entry '", "' for key")
+		return resterrors.NewBadRequestError(fmt.Sprintf("The %s %s already exists", duplicatedKey, duplicatedKeyValue))
+	}
+
+	return resterrors.NewInternalServerError(
+		fmt.Sprintf("Error trying to processing database request: %s", err.Error()))
+}
+
+func between(value string, a string, b string) string {
+	// Get substring between two strings.
+	posFirst := strings.Index(value, a)
+	if posFirst == -1 {
+		return ""
+	}
+	posLast := strings.Index(value, b)
+	if posLast == -1 {
+		return ""
+	}
+	posFirstAdjusted := posFirst + len(a)
+	if posFirstAdjusted >= posLast {
+		return ""
+	}
+	return value[posFirstAdjusted:posLast]
+}
+
+func before(value string, a string) string {
+	// Get substring before a string.
+	pos := strings.Index(value, a)
+	if pos == -1 {
+		return ""
+	}
+	return value[0:pos]
+}
+
+func after(value string, a string) string {
+	// Get substring after a string.
+	pos := strings.LastIndex(value, a)
+	if pos == -1 {
+		return ""
+	}
+	adjustedPos := pos + len(a)
+	if adjustedPos >= len(value) {
+		return ""
+	}
+	return value[adjustedPos:]
+}
